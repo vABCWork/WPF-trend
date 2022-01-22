330e1727afc25137b25fe03c1a88c62126875dc7
vABCWork <97317827+vABCWork@users.noreply.github.com>
2022-01-22 16:16:51 +0900

    Add files via upload

---
+++ CommLog.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Trend
+{
+
+    // 送信文表示用のクラス
+    public class SendFrame
+    {
+        public string msg { get; set; }   // 送信文
+        public override string ToString()
+        {
+            return msg;
+        }
+
+    }
+    // 受信文表示用のクラス
+    public class RcvFrame
+    {
+        public string msg { get; set; }   // 受信文
+        public override string ToString()
+        {
+            return msg;
+        }
+
+    }
+
+    /// <summary>
+    /// CommLog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CommLog : Window
+    {
+        public static ObservableCollection<SendFrame> sendframe_list;
+        public static ObservableCollection<RcvFrame> rcvframe_list;
+
+
+        public CommLog()
+        {
+            InitializeComponent();
+
+            sendframe_list = new ObservableCollection<SendFrame>();   // クラス SendFrameのコレクションをデータバインディングするため
+            rcvframe_list = new ObservableCollection<RcvFrame>();
+
+            this.SendmsgItems.ItemsSource = sendframe_list;
+            this.RcvmsgItems.ItemsSource = rcvframe_list;
+
+        }
+
+
+
+        // 送信データのリストへの追加
+        public static void sendmsg_disp()
+        {
+            if (MainWindow.send_msg_cnt > MainWindow.disp_msg_cnt_max) // 最大表示行数より大きい場合、リストをクリア
+            {
+                sendframe_list.Clear();
+                rcvframe_list.Clear();
+
+                MainWindow.send_msg_cnt = 0;
+            }
+
+            string msg = "";
+
+            for (int i = 0; i < MainWindow.sendByteLen; i++)
+            {
+                msg += MainWindow.sendBuf[i].ToString("X2") + " ";
+            }
+
+            msg += "(" + DateTime.Now.ToString("HH:mm:ss.fff") + ")" + "\r\n";
+
+            SendFrame sendFrame = new SendFrame();
+            sendFrame.msg = msg;
+
+            sendframe_list.Add(sendFrame);  　// Listに追加
+        }
+
+
+
+        // 受信データの表示
+        //
+        public static void rcvmsg_disp()
+        {
+            string rcv_str = "";
+
+            for (int i = 0; i < MainWindow.srcv_pt; i++)   // 表示用の文字列作成
+            {
+                rcv_str = rcv_str + MainWindow.rcvBuf[i].ToString("X2") + " ";
+            }
+
+            // 受信文と時刻
+           　rcv_str += "(" + MainWindow.receiveDateTime.ToString("HH:mm:ss.fff") + ")(" + MainWindow.srcv_pt.ToString() + " bytes )" + "\r\n";
+
+            RcvFrame rcvFrame = new RcvFrame();
+            rcvFrame.msg = rcv_str;
+
+            rcvframe_list.Add(rcvFrame);     // Listへ追加
+        }
+
+
+        // Windowが閉じられる際の処理
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow.commlog_window_cnt = 0;     // 通信ログ用ウィンドウの表示個数のクリア
+
+        }
+    }
+}
---
+++ ConfSerial.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Trend
+{
+
+    // COMポートの　コンボボックス用 
+    public class ComPortNameClass
+    {
+        string _ComPortName;
+
+        public string ComPortName
+        {
+            get { return _ComPortName; }
+            set { _ComPortName = value; }
+        }
+    }
+
+
+
+    /// <summary>
+    /// ConfSerial.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ConfSerial : Window
+    {
+
+
+        public ObservableCollection<ComPortNameClass> ComPortNames;    // 通信ポート(COM1,COM2等)のコレクション
+        public static SerialPort serialPort;        // シリアルポート
+        public ConfSerial()
+        {
+            InitializeComponent();
+
+            ComPortNames = new ObservableCollection<ComPortNameClass>();  // 通信ポートのコレクション　インスタンス生成
+
+            ComPortComboBox.ItemsSource = ComPortNames;       // 通信ポートコンボボックスのアイテムソース指定  
+
+            SetComPortName();                // 通信ポート名をコンボボックスへ設定
+
+            if (ComPortNames.Count > 0)     // 通信ポートがある場合
+            {
+                if (serialPort.IsOpen == true)    // new Confserial()実行時に、 既に Openしている場合
+                {
+                    OpenInfoTextBox.Text = "通信ポート(" + serialPort.PortName + ")は、既にオープンしています。";
+                    ComPortOpenButton.Content = "Close";      // ボタン表示 Close
+                }
+                else
+                {
+                    OpenInfoTextBox.Text = "通信ポート(" + serialPort.PortName + ")は、クローズしています。";
+                    ComPortOpenButton.Content = "Open";      // ボタン表示 Close
+                }
+            }
+            else
+            {
+                OpenInfoTextBox.Text = "通信ポートが見つかりません。";
+            }
+
+        }
+
+
+        // 通信ポート名をコンボボックスへ設定
+        private void SetComPortName()
+        {
+            ComPortNames.Clear();           // 通信ポートのコレクション　クリア
+
+
+            string[] PortList = SerialPort.GetPortNames();              // 存在するシリアルポート名が配列の要素として得られる。
+
+            foreach (string PortName in PortList)
+            {
+                ComPortNames.Add(new ComPortNameClass { ComPortName = PortName }); // シリアルポート名の配列を、コレクションへコピー
+            }
+
+            if (ComPortNames.Count > 0)
+            {
+                ComPortComboBox.SelectedIndex = 0;   // 最初のポートを選択
+
+                ComPortOpenButton.IsEnabled = true;  // ポートOPENボタンを「有効」にする。
+            }
+            else
+            {
+                ComPortOpenButton.IsEnabled = false;  // ポートOPENボタンを「無効」にする。
+            }
+
+        }
+
+
+
+        // 通信ポートの検索
+        //
+        private void ComPortSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetComPortName();
+        }
+
+
+        //
+        // 通信ポートのオープン
+        //
+        //  SerialPort.ReadBufferSize = 4096 byte (デフォルト)
+        //             WriteBufferSize =2048 byte
+        //
+        private void ComPortOpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (serialPort.IsOpen == true)    // 既に Openしている場合
+            {
+                try
+                {
+                    serialPort.Close();
+
+                    OpenInfoTextBox.Text = "通信ポート(" + serialPort.PortName + ")を、クローズしました。";
+
+                    ComPortComboBox.IsEnabled = true;        // 通信条件等を選択できるようにする。
+                    ComPortSearchButton.IsEnabled = true;    // 通信ポート検索ボタンを有効とする。
+                    ComPortOpenButton.Content = "Open"; 　　 // ボタン表示を Closeから Openへ
+
+                }
+                catch (Exception ex)
+                {
+                    OpenInfoTextBox.Text = ex.Message;
+                }
+
+            }
+            else                      // Close状態からOpenする場合
+            {
+                serialPort.PortName = ComPortComboBox.Text;    // 選択したシリアルポート
+
+                serialPort.BaudRate = 76800;           // ボーレート 76.8[Kbps]
+                serialPort.Parity = Parity.None;       // パリティ無し
+                serialPort.StopBits = StopBits.One;    //  1 ストップビット
+
+                serialPort.Open();             // シリアルポートをオープンする
+                serialPort.DiscardInBuffer();  // 受信バッファのクリア
+
+
+                ComPortComboBox.IsEnabled = false;        // 通信条件等を選択不可にする。
+
+                ComPortSearchButton.IsEnabled = false;    // 通信ポート検索ボタンを無効とする。
+
+                OpenInfoTextBox.Text = "通信ポート(" + serialPort.PortName + ")を、オープンしました。";
+
+                ComPortOpenButton.Content = "Close";      // ボタン表示を OpenからCloseへ
+
+            }
+        }
+
+        // OKボタン
+        private void ConfOKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
